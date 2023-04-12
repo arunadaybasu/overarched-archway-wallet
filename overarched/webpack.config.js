7e1e24999e407eee44ed03c3f360a3f3c961b456
Arunaday Basu <arunadaybasu@gmail.com>
2023-04-12 22:44:32 +0530

    Revert "Webpack and Assets"
    This reverts commit 71027e450738b67aa3c20ba45f065a398bb71ff9.

--- overarched/webpack.config.js
+++ overarched/webpack.config.js
@@ -1,18 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
-var fs = require('fs');
-
-// Look for .html files
-var htmlFiles = [];
-var directories = ['src'];
-while (directories.length > 0) {
-    var directory = directories.pop();
-    var dirContents = fs.readdirSync(directory)
-        .map(file => path.join(directory, file));
-
-    htmlFiles.push(...dirContents.filter(file => file.endsWith('.html')));
-    directories.push(...dirContents.filter(file => fs.statSync(file).isDirectory()));
-}
 
 module.exports = {
     resolve: {
@@ -34,46 +21,15 @@ module.exports = {
     },
     output: {
         filename: "bundle.js",
-        path: path.resolve(__dirname,  "dist"),
-        clean: true
+        path: path.resolve(__dirname,  "dist")
     },
     devServer: {
         port: 8081
     },
-    module: {
-        rules: [
-            {
-                test: /\.html$/i,
-                use: 'html-loader'
-            },
-            {
-                test: /\.(png|jpg)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'images/[name]-[hash][ext]'
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                  'style-loader',
-                  'css-loader'
-                ]
-            },
-            {
-                test: /\.svg$/,
-                use: "file-loader"
-            }
-        ]
-    },
     plugins: [
-        // Build a new plugin instance for each .html file found
-        ...htmlFiles.map(htmlFile =>
-            new HtmlWebpackPlugin({
-                template: htmlFile,
-                filename: htmlFile.replace(path.normalize("src/"), ""),
-                inject: false
-            })
-        )
+        new HtmlWebpackPlugin({
+            template: "index.html",
+            chunks: ["main"]
+        })
     ]
 };
