62b05b64c7e58b1d6910296bf589e7aca700d2eb
Arunaday Basu <arunadaybasu@gmail.com>
2023-04-14 02:07:27 +0530

    Webpack Image Loader
    - added image loader
    - added image rules
    - organized assets
    - html img tag working now

--- overarched/webpack.config.js
+++ overarched/webpack.config.js
@@ -25,10 +25,14 @@ module.exports = {
     },
     module: {
         rules: [
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader']
-          }
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            }
         ]
     },
     devServer: {
